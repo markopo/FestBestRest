b4c9bbb4fac731a6e198d2b3a4540314e90ef477
markopedu <69790644+markopedu@users.noreply.github.com>
2023-03-02 18:26:28 +0100

    Initial Commit

---
+++ FestBestRest/FestBestRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FestBestRestApp.swift
+//  FestBestRest
+//
+//  Created by Marko Poikkimäki on 2023-03-02.
+//
+
+import SwiftUI
+
+@main
+struct FestBestRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
