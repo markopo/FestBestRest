0c1dd9034faa0210b2b198621223f49b6e0dd75b
markopedu <69790644+markopedu@users.noreply.github.com>
2023-03-05 12:30:15 +0100

    first commit

--- FestBestRest/ContentView.swift
+++ FestBestRest/ContentView.swift
@@ -5,17 +5,61 @@
 //  Created by Marko Poikkimäki on 2023-03-02.
 //
 
+import CoreML
 import SwiftUI
 
+
+
+
+
 struct ContentView: View {
+    @State private var coffeAmount = 1
+    @State private var sleepAmount = 8.0
+    @State private var wakeUp = Date.now
+    
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                
+                Stepper("\(sleepAmount.formatted())", value: $sleepAmount, in: 4...12, step: 0.25)
+                    .padding(.all)
+                
+                Text("Daily coffee intake")
+                    .font(.headline)
+                
+                Stepper(coffeAmount == 1 ? "1 cup" : "\(coffeAmount) cups", value: $coffeAmount, in: 1...20)
+                    .padding(.all)
+            }
         }
-        .padding()
+        .navigationTitle("Fest Best Rest")
+        .toolbar {
+            Button("Calculate", action: calculateBedTime)
+        }
+    }
+    
+    func calculateBedTime() {
+        print("calculateBedTime!")
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            
+            
+        } catch {
+            print("exception: \(error)")
+        }
+        
     }
 }
 
