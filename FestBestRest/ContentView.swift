5caae6c5bb7001d90928f68d7a51e04c5416561d
markopedu <69790644+markopedu@users.noreply.github.com>
2023-03-05 12:50:09 +0100

    alert & calculate

--- FestBestRest/ContentView.swift
+++ FestBestRest/ContentView.swift
@@ -17,6 +17,9 @@ struct ContentView: View {
     @State private var sleepAmount = 8.0
     @State private var wakeUp = Date.now
     
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
     
     
     var body: some View {
@@ -40,11 +43,19 @@ struct ContentView: View {
                 Stepper(coffeAmount == 1 ? "1 cup" : "\(coffeAmount) cups", value: $coffeAmount, in: 1...20)
                     .padding(.all)
             }
+            .navigationTitle("Fest Best Rest")
+            .toolbar {
+                Button("Calculate", action: calculateBedTime)
+            }
+            .alert(alertTitle, isPresented: $showingAlert){
+                Button("OK") {
+                    
+                }
+            } message: {
+                Text(alertMessage)
+            }
         }
-        .navigationTitle("Fest Best Rest")
-        .toolbar {
-            Button("Calculate", action: calculateBedTime)
-        }
+      
     }
     
     func calculateBedTime() {
@@ -54,10 +65,21 @@ struct ContentView: View {
             let model = try SleepCalculator(configuration: config)
             
             let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
             
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            showingAlert = true
+            alertTitle = "Your ideal bedtime is..."
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
             
         } catch {
-            print("exception: \(error)")
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
+            showingAlert = true
         }
         
     }
